bb04abcb7085bde702d06f16f7237cdd1e12dc15
Prithvi333 <pthakur.pt36@gmail.com>
2026-06-14 16:26:42 +0530

    some configurations changes had done

--- services/user/src/main/java/com/nexora/user/config/kafka/KafkaProducerConfig.java
+++ services/user/src/main/java/com/nexora/user/config/kafka/KafkaProducerConfig.java
@@ -21,7 +21,7 @@ public class KafkaProducerConfig {
 
         config.put(
                 ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
+                "kafka:9092");
 
         config.put(
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
